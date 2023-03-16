589faadf888b5bb5ae6240a4efad3689b4658db2
Farhan Ishraq <omifarhan@gmail.com>
2023-03-16 19:54:22 +0600

    initial url setup

---
+++ WebApp/views.py
@@ -0,0 +1,10 @@
+from django.contrib.sites import requests
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h1> TwitterGPT Starts Here! </h1>")
